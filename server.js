6ee71e8543257ec2e30212605df934f437541be5
DarushDev <darushfaqiri@gmail.com>
2017-09-04 17:12:59 +0800

    Query the aws database and log the data

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var aws = require('aws-sdk'), simpledb;
 
-aws.config.loadFromPath(process.env['HOME'] + '/aws.credentials.json');
+aws.config.loadFromPath('./aws.credentials.json');
 
 //We'll use the Northern Virginia datacenter, change the region / endpoint for other datacenters http://docs.aws.amazon.com/general/latest/gr/rande.html#sdb_region
 simpledb = new aws.SimpleDB({
     region: 'US-East',
     endpoint: 'https://sdb.amazonaws.com'
+});
+
+simpledb.select({
+    // use your own aws database name instead of 'sdb-rest-darush'
+    SelectExpression  : 'select * from `sdb-rest-darush` limit 100'
+}, function(err,resp) {
+    if (err) {
+        console.error(err);
+    } else {
+        //print the data into console
+        console.log(JSON.stringify(resp,null,' '));
+    }
 });
